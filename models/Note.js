d3fb5fa5924057bc5116e893fd1d38baf2f3c6cb
Toxic254 <cyrus_g@outlook.com>
2022-12-21 16:30:24 +0300

    Created our Notes model associated with our User

---
+++ models/Note.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import AutoInc from "mongoose-sequence";
+import dbConn from "../config/dbConn.js";
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const AutoIncFactory = AutoInc(mongoose);
+
+noteSchema.plugin(AutoIncFactory, {
+  inc_field: "ticket",
+  id: "ticketNums",
+  start_seq: 500,
+});
+
+export default mongoose.model("Note", noteSchema);
