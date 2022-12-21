e82e2958c9d1c7a4ed853a8b12f4f85cf2b58e5c
Toxic254 <cyrus_g@outlook.com>
2022-12-21 16:30:37 +0300

    Created our User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    roles: [
+      {
+        type: String,
+        default: "Employee",
+      },
+    ],
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
